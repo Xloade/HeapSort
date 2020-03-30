4f5d5b208da91ee859627743ed8b8b0b5f5e6179
Dainius <dainius1999@hotmail.lt>
2020-03-30 23:42:12 +0300

    Prepare for disk

---
+++ HeapSort/TreeDisk.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.IO;
+using static System.Math;
+namespace HeapSort
+{
+    // class TreeDisk : Tree
+    // {
+    // }
+}
